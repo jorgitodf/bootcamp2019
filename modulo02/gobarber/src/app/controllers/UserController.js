8ecdbd5f4247f33c980bf2216c941fd8fd112b79
Jorgito Paiva <jspaiva.a977@gmail.com>
2019-10-21 16:20:07 -0300

    Configurando MongoDb e Mongoose

--- modulo02/gobarber/src/app/controllers/UserController.js
+++ modulo02/gobarber/src/app/controllers/UserController.js
@@ -52,6 +52,7 @@ class UserController {
         }
 
         const { email, oldPassword } = req.body;
+        console.log(req.userId);
         const user = await User.findByPk(req.userId);
 
         if (email !== user.email) {
--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -1,19 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
+import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 
-/* routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Jorgito da Silva Paiva',
-        email: 'jorgito@gmail.com',
-        password_hash: '123456',
-    });
-
-    return res.json(user);
-}); */
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -21,5 +19,14 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
