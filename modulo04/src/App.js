cf8818bc15a802a2ee86e73c01236c31117c0f94
jorgitodf <jspaiva.1977@gmail.com>
2019-10-27 09:33:33 -0300

    Iniciando com ReactJS

---
+++ modulo04/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css'
+import foto from './assets/IMG-20170818-WA0025.jpg';
+import TechList from './components/TechList';
+
+function App() {
+    return <TechList/>
+}    
+
+export default App;
